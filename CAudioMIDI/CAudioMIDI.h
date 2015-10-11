f54b6e90960ae9d90396aece62420b518d513bba
charlesgillingham <cgillingham1@mac.com>
2015-10-10 20:15:31 -0500

    Initial Commit

---
+++ CAudioMIDI/CAudioMIDI.h
@@ -0,0 +1,19 @@
+//
+//  CAudioMIDI.h
+//  CAudioMIDI
+//
+//  Created by CHARLES GILLINGHAM on 10/10/15.
+//  Copyright (c) 2015 CharlesGillingham. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CAudioMIDI.
+FOUNDATION_EXPORT double CAudioMIDIVersionNumber;
+
+//! Project version string for CAudioMIDI.
+FOUNDATION_EXPORT const unsigned char CAudioMIDIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CAudioMIDI/PublicHeader.h>
+
+
